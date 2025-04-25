dc6aeec454674dc5524da67e89a868ee9bcf416a
namna-0 <nmnaa0421@gmail.com>
2025-04-25 01:52:07 -0700

    Delete food-API directory

--- food-API/src/controllers/categoriesController.ts/getCategoryController.ts
+++
@@ -1,6 +0,0 @@
-import { categoryModel } from "../../models/foodCategory.model";
-
-export const getCategoryController = async (req, res) => {
-  const categories = await categoryModel.find({});
-  return res.status(200).json({ categories });
-};
--- food-API/src/controllers/foodController/get-food-by-id.ts
+++
@@ -1,7 +0,0 @@
-import { foodModel } from "../../models/food.model";
-
-export const getFoodController = async (req, res) => {
-  const { id } = req.body;
-  const foods = await foodModel.findById(id);
-  return res.status(200).json({ foods });
-};
